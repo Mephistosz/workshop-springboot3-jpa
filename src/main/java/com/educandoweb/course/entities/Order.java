2aec56bc9d3f0701d93e97ff35a54c49ae085005
felipe Gomes <Felipe.fg1@outlook.com>
2023-01-07 03:32:21 -0300

    Category

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -18,9 +18,6 @@ import jakarta.persistence.Table;
 @Table(name = "tb_order")
 public class Order implements Serializable {
 
-  /**
-   *
-   */
   private static final long serialVersionUID = 1L;
 
   @Id
